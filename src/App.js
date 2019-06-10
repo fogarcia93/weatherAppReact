a73d03e1fd2cade83041dea373c55cc467bd02a8
fogarcia93 <fogarcia10@gmail.com>
2019-06-09 23:21:51 -0600

    add redux to project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
-import Paper from '@material-ui/core/Paper';
-import AppBar from '@material-ui/core/AppBar';
-import  Typography from '@material-ui/core/Typography';
-import Toolbar from '@material-ui/core/Toolbar';
-import { Grid, Row, Col } from 'react-flexbox-grid';
-import LocationList from './components/LocationList';
-import ForecastExtended from './components/ForecastExtended';
+import React, { Component } from "react";
+import Paper from "@material-ui/core/Paper";
+import AppBar from "@material-ui/core/AppBar";
+import Typography from "@material-ui/core/Typography";
+import Toolbar from "@material-ui/core/Toolbar";
+import { Grid, Row, Col } from "react-flexbox-grid";
+import { createStore } from "redux";
+import LocationList from "./components/LocationList";
+import ForecastExtended from "./components/ForecastExtended";
 import "./App.css";
 
 const cities = [
@@ -16,42 +17,48 @@ const cities = [
   "Madrid,es",
   "San Pedro Sula,hn,"
 ];
+const store = createStore(() => {}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
-     this.state = { city: null };
+    this.state = { city: null };
     
   }
 
   handleSelectionLocation = city => {
-    this.setState({city});
+    this.setState({ city });
     console.log(`handleSelectionLocation ${city}`);
+
+    const action = { type: 'setCity', value: city};
+    store.dispatch(action);
+    
   };
   render() {
-    const {city} = this.state;
+    const { city } = this.state;
     return (
       <Grid>
         <Row>
-          <AppBar position='sticky'>
+          <AppBar position="sticky">
             <Toolbar>
-              <Typography variant='title' color='inherit'>
+              <Typography variant="title" color="inherit">
                 Weather App
               </Typography>
             </Toolbar>
           </AppBar>
         </Row>
         <Row>
-          <Col xs={12} md={6} >
-          <LocationList
-          cities={cities}
-          onSelectedLocation={this.handleSelectionLocation}/>
+          <Col xs={12} md={6}>
+            <LocationList
+              cities={cities}
+              onSelectedLocation={this.handleSelectionLocation}
+            />
           </Col>
-          <Col xs={12} md={6} >
+          <Col xs={12} md={6}>
             <Paper elevation={4}>
-            <div className="details">
-              {city && <ForecastExtended city={city}></ForecastExtended>}
-            </div>
+              <div className="details">
+                {city && <ForecastExtended city={city} />}
+              </div>
             </Paper>
           </Col>
         </Row>
