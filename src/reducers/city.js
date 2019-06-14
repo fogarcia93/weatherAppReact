e27787b0557e9d65bcf9bda3753b46c99322f449
fogarcia93 <fogarcia10@gmail.com>
2019-06-14 01:19:08 -0600

    remplace

--- src/reducers/city.js
+++ src/reducers/city.js
@@ -3,7 +3,7 @@ import { SET_CITY } from "./../actions";
 export const city = (state={}, action) => {
   switch (action.type) {
     case SET_CITY:
-      return action.payload ;
+      return { ...state, city: action.payload };
     default:
       return state;
   }
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,9 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from 'redux-thunk';
-import reducers from "./../reducers";
+import { createStore } from "redux";
+import { city } from './../reducers/city';
 
-const initialState = {
-  city: "San Pedro Sula,hn"
+const initialState ={
+    city: 'San Pedro Sula,hn'
 };
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-export const store = createStore(reducers, initialState, composeEnhancers(applyMiddleware(thunk)));
-
+export const store = createStore(city, initialState,
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
